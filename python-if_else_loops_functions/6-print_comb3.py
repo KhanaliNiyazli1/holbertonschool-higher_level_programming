00e71a6a36c10e89a9232090689f40bca3e248fa
KhanaliNiyazli1 <niyazlikhanali@gmail.com>
2024-11-18 14:33:25 +0400

    dsfds

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for i in range (0,10):
-    for j in range (0, 10):
+for i in range(0, 10):
+    for j in range(0, 10):
         if i == j:
             continue
         elif i < j:
-            print('{}{}'.format(i, j), end = ", " if i < 8 else "\n")
-        else:
-            continue
+            print('{}{}'.format(i, j), end=", " if i < 8 else "\n")
