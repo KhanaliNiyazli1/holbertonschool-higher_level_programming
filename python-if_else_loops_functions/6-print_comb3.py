d58ebe20420aacacd4a0736b59743fe2ad68db2a
KhanaliNiyazli1 <niyazlikhanali@gmail.com>
2024-11-18 14:31:56 +0400

    daf

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range (0,10):
         if i == j:
             continue
         elif i < j:
-            print('{}{}'.format(i, j), end=", " if i < 8 else "\n")
+            print('{}{}'.format(i, j), end = ", " if i < 8 else "\n")
         else:
             continue
