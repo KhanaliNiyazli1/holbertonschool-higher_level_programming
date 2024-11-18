2964b589e69d1a62a556c76a9a95e30bd0983efa
KhanaliNiyazli1 <niyazlikhanali@gmail.com>
2024-11-18 14:54:49 +0400

    fix no idk

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
-    if not isinstance(c, str) or len(c) != 1:
-        return ord('a') <= ord(c) <= ord('z') and isinstance(c, str) and len(c) == 1
+    return ord('a') <= ord(c) <= ord('z') and isinstance(c, str) and len(c) == 1
+
