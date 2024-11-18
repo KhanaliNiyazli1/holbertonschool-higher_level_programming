1ae0c2ee0943a43d66d4e4afd6c31c1e4828882c
KhanaliNiyazli1 <niyazlikhanali@gmail.com>
2024-11-18 14:58:37 +0400

    df

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z') and isinstance(c, str) and len(c) == 1
+    return (ord('a') <= ord(c) <= ord('z')) and isinstance(c, str) and len(c) == 1
