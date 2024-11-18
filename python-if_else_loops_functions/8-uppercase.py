2eb93e5e93c0e4b920bfe412b864cdd9644d31b9
KhanaliNiyazli1 <niyazlikhanali@gmail.com>
2024-11-18 15:22:49 +0400

    fix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in str:
         if ord('a') <= ord(i) <= ord('z'):
             i = ord(i-32)
-            return i
+            print(i)
         else:
-            return i
+            print(i)
