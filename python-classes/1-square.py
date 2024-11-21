6d4136e6998cf8b28cd3da6f28f4ee77790f4c68
KhanaliNiyazli1 <niyazlikhanali@gmail.com>
2024-11-21 16:15:49 +0400

    task1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,5 +6,12 @@ This module defines an empty class Square.
 
 class Square:
     """A class that defines a square with its size."""
-    def __init__(self, __Square_size):
-        self.size = __Square_size
+    def __init__(self, size):
+        """
+        Initialize the square.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
+
